88bd8fb9029a5d36a0bbc7206a7c305f4c2bfb4a
Unknown <steppi.thiel@googlemail.com>
2018-03-23 10:44:17 +0100

    stuff

--- src/me/tangledmazes/spthiel/commands/CommandListener.java
+++ src/me/tangledmazes/spthiel/commands/CommandListener.java
@@ -15,61 +15,33 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.permissions.Permission;
 
+import me.tangledmazes.main.TangledMain;
+
 public class CommandListener implements CommandExecutor {
 	
-	private ItemStack wand;
-
-	public CommandListener() {
-		
-		wand = new ItemStack(Material.GOLD_SPADE);
-		wand.setAmount(1);
-		wand.setDurability((short)1);
-		ItemMeta wandmeta = wand.getItemMeta();
-		wandmeta.setDisplayName("§fSelection Shovel");
-		wandmeta.addEnchant(Enchantment.ARROW_INFINITE,1,true);
-		ArrayList<String> lore = new ArrayList<>();
-		lore.add("§bUltimate tool for maze selections.");
-		lore.add("§bUse right click to set or move points.");
-		wandmeta.setLore(lore);
-		wand.setItemMeta(wandmeta);
-	}
 	
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String name, String[] args) {
 		
 		
-		if(commandSender instanceof ConsoleCommandSender) {
+		if (commandSender instanceof ConsoleCommandSender) {
 			return true;
 		}
 		
-		Player p = (Player)commandSender;
+		Player p = (Player) commandSender;
 		
-		if(command.getName().equalsIgnoreCase("tangledmaze")) {
-			if(args.length > 0) {
-				switch(args[0].toLowerCase()) {
+		if (command.getName().equalsIgnoreCase("tangledmaze")) {
+			if (args.length > 0) {
+				switch (args[0].toLowerCase()) {
 					case "wand":
 						
-						String NAME = "§dSelection Shovel";
 						
-						for(ItemStack i : p.getInventory().getContents()) {
-							if(i != null && i.hasItemMeta() && i.getItemMeta().getDisplayName().equalsIgnoreCase(NAME))
+						for (ItemStack i : p.getInventory().getContents()) {
+							if (i != null && TangledMain.isSelectionWand(i))
 								p.getInventory().remove(i);
 						}
 						
-						ItemStack wand = new ItemStack(Material.GOLD_SPADE);
-						wand.setAmount(1);
-						wand.setDurability((short)1);
-						ItemMeta wandmeta = wand.getItemMeta();
-						wandmeta.setDisplayName(NAME);
-						wandmeta.addEnchant(Enchantment.ARROW_INFINITE,1,true);
-						wandmeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-						ArrayList<String> lore = new ArrayList<>();
-						lore.add("§7" + getCustomEnchanment());
-						lore.add("§bUltimate tool for maze selections.");
-						lore.add("§bUse right click to set or move points.");
-						wandmeta.setLore(lore);
-						wand.setItemMeta(wandmeta);
-						p.getInventory().addItem(wand);
+						p.getInventory().addItem(TangledMain.getWand());
 						break;
 					case "undo":
 						break;
@@ -78,79 +50,38 @@ public class CommandListener implements CommandExecutor {
 					case "help":
 					case "?":
 					case "h":
-						if(args.length > 2) {
+						if (args.length > 2) {
 							try {
 								int page = Integer.parseInt(args[2]);
-								sendHelp(p,page);
-							} catch(NumberFormatException e) {
-								sendHelp(p,1);
+								sendHelp(p, page);
+							} catch (NumberFormatException e) {
+								sendHelp(p, 1);
 							}
 						} else {
-							sendHelp(p,1);
+							sendHelp(p, 1);
 						}
 						break;
 					default:
-						sendHelp(p,1);
+						sendHelp(p, 1);
 				}
 			} else {
-				sendHelp(p,1);
-
-			switch(args[0].toLowerCase()) {
-				case "wand":
-					p.getInventory().addItem(wand.clone());
-					break;
-				case "undo":
-					break;
-				case "deselect":
-					break;
-				case "help":
-				case "?":
-				case "h":
-					if(args.length < 3)
-						sendHelp(p, 1);
-
-					try {
-						int page = Integer.parseInt(args[2]);
-						sendHelp(p,page);
-					}catch(NumberFormatException e) {
-						sendHelp(p, 1);
-					}
-					break;
-				default:
-					sendHelp(p, 1);
+				sendHelp(p, 1);
 			}
 		}
 		return false;
 	}
 	
-	@SuppressWarnings("unused")
+	
 	private void hasAnyPermission(Permission... perms) {
 	
 	}
 	
-	@SuppressWarnings("unused")
 	private void hasAllPermissions(Permission... perms) {
 	
 	}
 	
-	private void sendHelp(Player p,int page) {
+	private void sendHelp(Player p, int page) {
 		p.sendMessage("Help");
 	}
-	
-	private static final String[] ench = {
-			"Selector I",
-			"Selector II",
-			"Selector III",
-			"Infinite Maze I",
-			"Ban hammer I",
-			"Force OP I",
-			"Java.exe II",
-			"Ignore WorldGuard IV"
-	};
-	private static final Random   rnd  = new Random();
-	
-	private String getCustomEnchanment() {
-		int select = rnd.nextInt(ench.length);
-		return ench[select];
-	}
+
 }
